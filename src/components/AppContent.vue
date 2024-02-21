920e0f8f90f543a777e347529baae7e86a8f4905
haguri <hty851111@gmail.com>
2024-02-22 00:36:04 +0900

    modify adsense_multiflex's style

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -84,7 +84,7 @@
     <!-- adsense_multiflex -->
     <ins
       class="adsbygoogle"
-      style="display: inline-block; width: 360px; height: 800px"
+      style="display: inline-block; width: 728px; height: 400px"
       data-ad-client="ca-pub-6187932165258053"
       data-ad-slot="2087795028"
     ></ins>
