d81ef0bc886aa4e0586d9b34368e3fc41ca43a09
haguri <hty851111@gmail.com>
2024-11-24 10:14:50 +0900

    css, slideUp, toggle 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -77,6 +77,83 @@ const setValue = <T>(obj: T, key: keyof T, value: T[keyof T]) => {
   obj[key] = value;
 };
 
+type StandardCSSProperties = {
+  [K in keyof CSSStyleDeclaration]?: string | number;
+};
+interface CustomCSSProperties {
+  [key: string]: string | number | undefined;
+}
+type CSSProperties = StandardCSSProperties & CustomCSSProperties;
+const css = (
+  element: HTMLElement | NodeListOf<HTMLElement>,
+  styles: string | CSSProperties,
+): string | void => {
+  if (element instanceof HTMLElement) {
+    if (typeof styles === 'string') {
+      return window.getComputedStyle(element)[styles as any];
+    }
+    Object.assign(element.style, styles);
+  } else {
+    // NodeListOf
+    // 이 경우에는 styles에 string이 들어오는 경우 빈 값('')만 리턴
+    if (typeof styles === 'string') {
+      return '';
+    }
+    element.forEach((el) => {
+      Object.assign(el.style, styles);
+    });
+  }
+};
+
+/**
+ * 요소를 위로 슬라이드하며 숨기는 애니메이션 함수
+ * @param element - 애니메이션을 적용할 HTML 요소
+ * @param duration - 애니메이션 지속 시간 (ms)
+ * @param easing - 애니메이션 이징 함수
+ */
+const slideUp = (
+  element: HTMLElement,
+  duration: number = 300,
+  easing: string = 'ease-in-out',
+): void => {
+  // 현재 요소의 높이 저장
+  const height = element.offsetHeight;
+
+  // 트랜지션 설정
+  element.style.transition = `height ${duration}ms ${easing}, 
+                              padding ${duration}ms ${easing}, 
+                              margin ${duration}ms ${easing}`;
+  element.style.height = `${height}px`;
+
+  // 다음 프레임에서 높이를 0으로
+  requestAnimationFrame(() => {
+    element.style.height = '0';
+    element.style.paddingTop = '0';
+    element.style.paddingBottom = '0';
+    element.style.marginTop = '0';
+    element.style.marginBottom = '0';
+    element.style.overflow = 'hidden';
+  });
+
+  // 애니메이션 완료 후 요소 숨김
+  setTimeout(() => {
+    element.style.display = 'none';
+    element.style.height = 'auto';
+    element.style.padding = 'auto';
+    element.style.paddingTop = '60px';
+    element.style.margin = 'auto';
+    element.style.overflow = '';
+  }, duration);
+};
+
+const toggle = (element: HTMLElement) => {
+  if (element.style.display === 'none') {
+    element.style.display = '';
+  } else {
+    element.style.display = 'none';
+  }
+};
+
 export {
   isMobile,
   isNullStr,
@@ -84,4 +161,7 @@ export {
   commentReduce,
   handleNewLine,
   setValue,
+  css,
+  slideUp,
+  toggle,
 };
